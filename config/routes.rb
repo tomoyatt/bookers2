b0fafcb99d216760a4dcb581a81407c543183745
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-05-17 14:47:17 +0000

    [Add] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/index'
+  get 'books/show'
+  devise_for :users
+  root to: "homes#top"
+  get "homes/about" => "homes#about",as: 'about'
 end
