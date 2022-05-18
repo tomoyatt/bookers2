5ab9b899335c5efc590f9af3ba79fe7b1e8dc681
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-05-18 14:33:46 +0000

    [Add] about

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about",as: 'about'
+  resources :books, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit]
 end
